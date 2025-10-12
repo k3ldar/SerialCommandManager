f5e7b0b3eab0d5cfbc690ce3774a10c812c34cfc
Si Carter <s1cart3r@gmail.com>
2025-10-12 17:31:11 +0200

    Update sample project

--- examples/Example/Example.ino
+++ examples/Example/Example.ino
@@ -4,56 +4,49 @@
 // ------------------------------
 // Example command handlers
 // ------------------------------
+#include "SerialCommandManager.h"
 
-// DEBUG command handler
-void handleDebug(const String& command, StringKeyValue* params, int paramCount, void* context)
-{
-    // remove warning for unused params
-    (void)command;
-    (void)context;
-    
-    if (paramCount > 0 && params[0].key.equalsIgnoreCase("ON"))
-        Serial.println("Debug mode ENABLED");
-    else if (paramCount > 0 && params[0].key.equalsIgnoreCase("OFF"))
-        Serial.println("Debug mode DISABLED");
-}
+// example commands would be
+// MOVE:direction=REVERSE;speed=180;
+class MotorHandler : public ISerialCommandHandler {
+private:
+    const String _supportedCommands[1] = {"MOVE"};
+    int _pinDirection;
+    int _pinSpeed;
 
-// LED command handler
-void handleLED(const String& command, StringKeyValue* params, int paramCount, void* context)
-{
-    // remove warning for unused params
-    (void)command;
-    (void)context;
-
-    int pin = 13;
-    bool state = false;
-
-    for (int i = 0; i < paramCount; i++)
-    {
-        if (params[i].key.equalsIgnoreCase("pin")) pin = params[i].value.toInt();
-        if (params[i].key.equalsIgnoreCase("state")) state = params[i].value.equalsIgnoreCase("ON");
+public:
+    MotorHandler(int directionPin, int speedPin)
+        : _pinDirection(directionPin), _pinSpeed(speedPin) {
+        pinMode(_pinDirection, OUTPUT);
+        pinMode(_pinSpeed, OUTPUT);
     }
 
-    pinMode(pin, OUTPUT);
-    digitalWrite(pin, state ? HIGH : LOW);
+    void handleCommand(SerialCommandManager* sender, const String command, const StringKeyValue params[], int paramCount) override {
+        Serial.println(sender->getRawMessage());
+        (void)command;
+        String direction = "FORWARD";
+        int speed = 0;
 
-    Serial.print("LED command processed: pin=");
-    Serial.print(pin);
-    Serial.print(", state=");
-    Serial.println(state ? "ON" : "OFF");
-}
+        for (int i = 0; i < paramCount; ++i) {
+            if (params[i].key == "direction") {
+                direction = params[i].value;
+            } else if (params[i].key == "speed") {
+                speed = params[i].value.toInt();
+            }
+        }
 
-// PING command handler
-void handlePing(const String& command, StringKeyValue* params, int paramCount, void* context)
-{
-    // remove warning for unused params
-    (void)command;
-    (void)params;
-    (void)paramCount;
-    (void)context;
+        digitalWrite(_pinDirection, direction == "FORWARD" ? HIGH : LOW);
+        analogWrite(_pinSpeed, constrain(speed, 0, 255));
 
-   Serial.println("PONG;");
-}
+        sender->sendDebug("Motor moved " + direction + " at speed " + String(speed), "MotorHandler");
+    }
+
+    const String* supportedCommands(size_t& count) const override {
+        count = 1;
+        return _supportedCommands;
+    }
+};
+
 
 // Fallback handler for unrecognized commands
 void handleUnknown(SerialCommandManager* mgr)
@@ -67,35 +60,28 @@ void handleUnknown(SerialCommandManager* mgr)
 // ------------------------------
 // Create SerialCommandManager
 // ------------------------------
-SerialCommandManager commandMgr(
-    &Serial,          // Serial port
-    handleUnknown,    // Default fallback handler
-    '\n',             // Message terminator
-    ':',              // Command/parameter separator
-    '=',              // Key/value separator
-    500,              // Timeout in ms
-    256               // Maximum message size
-);
+MotorHandler motorHandler(5, 6); // Direction on pin 5, speed on pin 6
+
+SerialCommandManager commandManager(&Serial, nullptr, '\n', ':', ';', 1000, 64);
 
 // ------------------------------
 // Setup
 // ------------------------------
 void setup()
 {
+    ISerialCommandHandler* handlers[] = { &motorHandler };
+    commandManager.registerHandlers(handlers, 1);
+
     Serial.begin(115200);
     delay(500);
     Serial.println("SerialCommandManager Example Started");
 
-    // Register command handlers
-    commandMgr.registerHandler("DEBUG", handleDebug);
-    commandMgr.registerHandler("LED", handleLED);
-    commandMgr.registerHandler("PING", handlePing);
 
     Serial.println("Ready to receive commands...");
     Serial.println("Examples:");
     Serial.println("  DEBUG:ON;");
-    Serial.println("  LED:pin=13,state=ON;");
-    Serial.println("  PING;");
+    Serial.println("  DEBUG:OFF");
+    Serial.println("  MOVE:direction=REVERSE;speed=180;");
 }
 
 // ------------------------------
@@ -104,13 +90,13 @@ void setup()
 void loop()
 {
     // Continuously read and process serial input
-    commandMgr.readCommands();
+    commandManager.readCommands();
 
     // Example of sending debug message periodically
     static unsigned long lastTime = 0;
     if (millis() - lastTime > 5000)
     {
         lastTime = millis();
-        commandMgr.sendDebug("Heartbeat alive", "Loop");
+        commandManager.sendDebug("Heartbeat alive", "Loop");
     }
 }
